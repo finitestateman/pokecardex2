bb3b2245ed96d48de1264e3876b3a0e41add0c49
Finite Stateman <finitestateman@gmail.com>
2025-02-26 01:16:12 +0900

    Update

--- newDDL.sql
+++ newDDL.sql
@@ -1,11 +1,13 @@
 -- enums
-create type type as enum ('grass', 'fire', 'water', 'lightning', 'psychic', 'fighting', 'darkness', 'metal', 'dragon', 'colorless');
+-- https://www.reddit.com/r/pkmntcg/comments/8bjpu1/color_identity_in_pkmntcg/
+-- create type tcg_type as enum ('grass', 'fire', 'water', 'lightning', 'psychic', 'fighting', 'darkness', 'metal', 'dragon', 'colorless');
+create type energy as enum ('grass', 'fire', 'water', 'lightning', 'psychic', 'fighting', 'darkness', 'metal', 'dragon', 'colorless');
+
 create type rpg_type as enum ('normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy', 'stellar');
 create type trainer as enum ('item', 'pokemon_tool', 'fossil', 'supporter');
 -- alter type vg_type rename to rpg_type;
-create type rarity as enum ('p', 'd1', 'd2', 'd3', 'd4', 's1', 's2', 's3', 'c');
+create type rarity as enum ('promo', 'dia1', 'dia2', 'dia3', 'dia4', 'star1', 'star2', 'star3', 'crown');
 -- promo -- common, uncommon, rare, double_rare, art_rare, super_rare, immserive_rare, crown_rare
-create type card_type as enum ('pokemon', 'trainer');
 
 create type operator as enum ('+', '*');
 
@@ -14,16 +16,25 @@ create domain generation as integer check (value between 1 and 9);
 create domain stage as integer check (value in (0, 1, 2));
 
 -- tables
+
+create table tcg_type
+(
+    id    integer primary key,
+    name  varchar not null,
+    color varchar not null
+);
+
 create table pokemon
 (
     id             integer generated always as identity primary key,
-    dex_no         integer  not null, -- 전국 도감 번호(0025)
-    species        varchar  not null, -- 'Mouse Pokémon'
-    name           varchar  not null, -- 'Pikachu'
-    type1          rpg_type not null, -- 'electric'::lightning
-    type2          rpg_type,          -- 두번째 타입은 없을 수도 있다
-    height         integer,           -- 40(cm)
-    weight         integer,           -- 6000(g)
+    dex_no         integer        not null, -- 전국 도감 번호(0025); not unique in the case of Rotom
+    species        varchar        not null, -- 'Mouse Pokémon'
+    identifier     varchar unique not null, -- e.g. 'nidoran-m', 'farfetchd'. Used as a URL path variable(kebab-case)
+    name           varchar unique not null, -- Original Eng. name(= display_name); e.g. 'Nidoran♂' (U+2642) or "Farfetch'd".
+    type1          rpg_type       not null,
+    type2          rpg_type,                -- 두번째 타입은 없을 수도 있다
+    height         integer,                 -- 40(cm)
+    weight         integer,                 -- 6000(g)
     introduced_gen generation,
     unique (dex_no, name)
 );
@@ -32,8 +43,8 @@ create table pokemon
 create table description
 (
     id         integer generated always as identity primary key,
-    content    text                                                                 not null,
-    pokemon_id integer references pokemon (id) on delete set null on update cascade not null unique
+    content    text    not null,
+    pokemon_id integer not null unique references pokemon (id) on delete set null on update cascade
 );
 
 create table illustrator
@@ -47,7 +58,7 @@ create table attack
     id          integer generated always as identity primary key,
     name        varchar unique not null, -- 'Thunderbolt'
     damage      integer,                 -- 140
-    energies    type[]         not null, -- ['lightning'::tcg_type, 'lightning'::tcp_type, 'lightning'::tcg_type]
+    energies    energy[]     not null, -- ['lightning'::energy, 'lightning'::energy, 'lightning'::energy]
     description text,                    -- 'Discard all Energy from this Pokémon.'
     operator    operator
 );
@@ -82,28 +93,31 @@ create table ability
 create table card
 (
     id             integer generated always as identity primary key,
-    name           varchar unique not null,
-    card_no        integer unique not null, -- 0026 카드가 속한 부스터팩 내부에서의 번호
+    identifier     varchar unique not null, -- internal_name e.g. 'nidoran-m', 'farfetchd'. Used as a URL path variable(kebab-case)
+    name           varchar unique not null, -- Original Eng. name(= display_name); e.g. 'Nidoran♂' (U+2642) or "Farfetch'd".
+    card_no        integer unique not null, -- Card number within the booster pack (e.g., 0026).
     rarity         rarity         not null,
+    point          integer,
+    offering_rate  numeric(6, 3),
     illustrator_id integer references illustrator (id)
 );
 
 create table pokemon_card
 (
     card_id        integer references card (id) on delete cascade primary key,
-    stage          stage                           not null, -- 0, 1, 2 진화 상태('Basic', 'Stage 1', 'Stage 2') 대신 숫자로 사용
-    hp             integer                         not null, -- 120
-    type           type                            not null, -- 'lightning'::tcg_type
-    attack1        integer references attack (id)  not null, -- n:1 -> 여러 포켓몬 카드가 하나의 첫번째 공격 기술을 참조함
-    attack2        integer references attack (id),           -- n:1 -> 여러 포켓몬 카드가 하나의 두번째 공격 기술을 참조할 수 있음(nullable)
-    retreat        type[]                          not null, -- ['colorless'::tcg_type, 'colorless'::tcg_type]
-    weakness       type                            not null,
-    ex_rule        text,                                     -- 'When your Pokémon ex is Knocked Out, your opponents gets 2 points.' 이 값이 존재하면 ex pokemon이므로 boolean처럼 사용. ex_text 값은 항상 같은 것 같다
+    stage          stage                            not null, -- 0, 1, 2 진화 상태('Basic', 'Stage 1', 'Stage 2') 대신 숫자로 사용
+    hp             integer                          not null, -- 120
+    type_id        integer references tcg_type (id) not null, -- n:1 -> 여러 포켓몬 카드가 하나의 타입을 공유할 수 있음
+    attack1        integer references attack (id)   not null, -- n:1 -> 여러 포켓몬 카드가 하나의 첫번째 공격 기술을 참조함
+    attack2        integer references attack (id),            -- n:1 -> 여러 포켓몬 카드가 하나의 두번째 공격 기술을 참조할 수 있음(nullable)
+    retreat        energy[]                       not null, -- ['colorless'::type, 'colorless'::type] -- retreat은 type보단 개수가 중요하고 조회용으로만 사용되므로 배열 사용
+    weakness       integer references tcg_type (id) not null, -- n:1(type_id와 동일)
+    ex_rule        text,                                      -- 'When your Pokémon ex is Knocked Out, your opponents gets 2 points.' 이 값이 존재하면 ex pokemon이므로 boolean처럼 사용. ex_text 값은 항상 같은 것 같다
     -- n:1 -> 여러 포켓몬 카드가 한 포켓몬으로부터 진화할 수 있음, 화석 포켓몬에 대한 예외처리 필요
     -- pokemon_id 참조하는 원리와 같음
     evolves_from   integer references pokemon (id),
     ability_id     integer references ability (id),
-    pokemon_id     integer references pokemon (id) not null, -- n:1 -> 여러 포켓몬 카드가 하나의 포켓몬을 참조할 수 있음
+    pokemon_id     integer references pokemon (id)  not null, -- n:1 -> 여러 포켓몬 카드가 하나의 포켓몬을 참조할 수 있음
     description_id integer references description (id)
 );
 
@@ -122,7 +136,7 @@ create table trainer_card
 (
     card_id     integer references card (id) on delete cascade primary key,
     kind        trainer not null, -- 'item', 'Pokémon Tool', 'Fossil', 'Supporter'
-    description text    not null, -- trainer_card는 description이 겹치는 경우가 희귀하기 때문에 별도 테이블을 두지 않음
+    description text    not null, -- trainer_card는 description이 겹치는 경우는 동일한 서포터 카드에 일러스트만 다른 경우밖에 없기 때문에 별도 테이블을 두지 않음
     footnote    text
 );
 
@@ -130,13 +144,6 @@ create table fossil_card
 (
     trainer_card_id integer references trainer_card (card_id) on delete cascade primary key,
     hp              integer not null
-);
-
-create table meta_card_info
-(
-    card_id       integer references pokemon_card (card_id) on delete cascade primary key,
-    point         integer,
-    offering_rate numeric(6, 3)
 );
 
 create table promo_card
@@ -167,9 +174,9 @@ create table cards_join_booster_packs
 create table deck
 (
     id              integer generated always as identity primary key,
-    deck_box        type,
-    energies        type[] not null,
-    highlight_cards integer[],                            -- references card (id[])
+    deck_box        energy,
+    energies        energy[] not null,
+    highlight_cards integer[],                                 -- references card (id[])
     pokemon_card_id integer references pokemon_card (card_id), -- 동일한 포켓몬인지 구분하는 방법 고민(피카츄가 2마리여도 ex면 추가가 가능하다)
     trainer_card_id integer references trainer_card (card_id)  -- trainer는 이름이 같으면 2개 중복을 비허용
 );
